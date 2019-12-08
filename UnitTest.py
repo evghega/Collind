9175fe08a8c4c03d1b0488d52ed3cf146cc9d848
Snir Ben Yossef <snirben@gmail.com>
2019-12-08 16:18:09 +0200

    xlsx and unittest update

--- UnitTest.py
+++ UnitTest.py
@@ -5,9 +5,9 @@ import check
 class TestStringMethods(unittest.TestCase):
 
     def test_excel_func(self):
-        self.assertTrue(check.checkAnswer(2,2,"A"),"Not True, check Cube")
+        self.assertTrue(check.checkAnswer(2,2,"B"),"Not True, check Cube")
         self.assertFalse(check.checkAnswer(2, 2, "A"), "Not False, check Cube or answer")
-
+        self.assertEqual(check.CreateGameID(),19,"check if number is updated to the next one")
 
 if __name__ == '__main__':
     unittest.main()
