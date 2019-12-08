c6d984435467e4a61c60cf4f18bc7c184b8780f7
Snir Ben Yossef <snirben@gmail.com>
2019-12-08 16:22:10 +0200

    change names

--- UnitTest.py
+++ UnitTest.py
@@ -1,13 +1,13 @@
 import unittest
-import check
+import excelfunc
 
 
 class TestStringMethods(unittest.TestCase):
 
     def test_excel_func(self):
-        self.assertTrue(check.checkAnswer(2,2,"B"),"Not True, check Cube")
-        self.assertFalse(check.checkAnswer(2, 2, "A"), "Not False, check Cube or answer")
-        self.assertEqual(check.CreateGameID(),19,"check if number is updated to the next one")
+        self.assertTrue(excelfunc.checkAnswer(2, 2, "B"), "Not True, check Cube")
+        self.assertFalse(excelfunc.checkAnswer(2, 2, "A"), "Not False, check Cube or answer")
+        self.assertEqual(excelfunc.CreateGameID(), 19, "check if number is updated to the next one")
 
 if __name__ == '__main__':
     unittest.main()
--- cardandcube.py
+++ cardandcube.py
@@ -1,5 +1,4 @@
 from random import randint
-#test
 def coub():
     
     num = randint(1,6) 
