2142c53ab6a36341a9d2d0e3389c304c73aa874b
Snir Ben Yossef <snirben@gmail.com>
2020-01-09 19:18:30 +0200

    Change Sql Add Gui סיפורי משתמש 1 11 4 5 6 7 8 9 10 112 13 14 15 16 17 18 19 20 וכו'

--- Main.py
+++ Main.py
@@ -42,9 +42,9 @@ password=""
 hash=""
 badpassword=False
 baduser=False
-
+subScreen_num = 0 #screen 3 helper
 pygame.display.set_caption("Collind")
-
+pos=0
 
 # functions
 def card(Card_Num):
@@ -102,26 +102,14 @@ while not done:
         Mouse_x = pos[0]
         Mouse_y = pos[1]
 
-    # --- Game logic should go here
-
-    # --- Screen-clearing code goes here
-
-    # Here, we clear the screen to white. Don't put other drawing commands
-    # above this, or they will be erased with this command.
-
-    # If you want a background image, replace this clear with blit'ing the
-    # background image.
-   # screen.fill(pygame.image.load(r'CARD5.png'))
     background_image = pygame.image.load("loginbackground.png").convert()
     badpassword_image = pygame.image.load("WorngPassword.png").convert()
     baduser_image = pygame.image.load("WorngUserName.png").convert()
     screen.blit(background_image, [0, 0])
     # --- Drawing code should go here
-    if screen_Num == 0:
+    if screen_Num == 0: #login page
         running = True
         while running:
-           # textinput.update(events)
-
          print(username)
          for evt in pygame.event.get():
             if evt.type == KEYDOWN:
@@ -134,15 +122,13 @@ while not done:
          if(baduser):
             screen.blit(baduser_image,[0, 0])
          if (badpassword):
-            screen.blit(baduser_image, [0, 0])
+            screen.blit(badpassword_image, [0, 0])
          block = font.render(username, True, (0, 0, 0))
-         if(not baduser and not baduser):
+         if(not baduser and not badpassword):
           screen.blit(background_image, [0, 0])
          screen.blit(block, [350, -50+Center_y + A_hig / 2])
          pygame.display.flip()
         while not running:
-            # textinput.update(events)
-
             print(password)
             for evt in pygame.event.get():
                 if evt.type == KEYDOWN:
@@ -163,21 +149,25 @@ while not done:
                             if(tuple[1]=='badpassword'):
                                 badpassword=True
                                 username=""
-                                hase=""
+                                hash=""
                                 password=""
                             else:
                                 baduser=True
                                 username = ""
-                                hase = ""
+                                hash= ""
                                 password = ""
 
                         if (tuple[0] == True):
                             if (tuple[1] == 'admin'):
                                 screen_Num = 1
+                            if (tuple[1] == 'tester'):
+                                screen_Num = 2
+                            if (tuple[1] == 'user'):
+                                screen_Num = 3
             if (baduser):
                 screen.blit(baduser_image, [0, 0])
             if (badpassword):
-                screen.blit(baduser_image, [0, 0])
+                screen.blit(badpassword_image, [0, 0])
             block = font.render(hash, True, (0, 0, 0))
             block2 = font.render(username, True, (0, 0, 0))
             if (not baduser and not baduser):
@@ -190,44 +180,52 @@ while not done:
     elif screen_Num == 1:
       manager_image=pygame.image.load("ManagerMain.png").convert()
       screen.blit(manager_image, [0, 0])
-    elif screen_Num == 3:
-        card(Card_Num)
-        A1 = pygame.draw.rect(screen, BLACK, [A1_x, A1_y, A_wid, A_hig], 2)
-        text = font.render("1", True, BLACK)
-        screen.blit(text, [A1_x + A_wid/2, A1_y + A_hig / 2])
-
-        A2 = pygame.draw.rect(screen, BLACK, [A2_x, A2_y, A_wid, A_hig], 2)
-        text = font.render("2", True, BLACK)
-        screen.blit(text, [A2_x + A_wid/2, A2_y + A_hig / 2])
-
-        A3 = pygame.draw.rect(screen, BLACK, [A3_x, A3_y, A_wid, A_hig], 2)
-        text = font.render("3", True, BLACK)
-        screen.blit(text, [A3_x + A_wid/2, A3_y + A_hig / 2])
-
-        P = pygame.draw.rect(screen, BLACK, [Pause_x, Pause_y, A_wid, A_hig], 2)
-        text = font.render("Pause", True, BLACK)
-        screen.blit(text, [Pause_x + 2, Pause_y + A_hig / 2])
-
-        if event.type == pygame.MOUSEBUTTONUP:
-            count = 0
-        if event.type == pygame.MOUSEBUTTONDOWN and count == 0:
-            Mouse_x, Mouse_y = event.pos
-            if A1.collidepoint(Mouse_x, Mouse_y):
-                excelfunc.answertoDB(excelfunc.checkAnswer(1, Card_Num, Cube_Num), GameID)
-                print("A1")
-                count = 1
-            if A2.collidepoint(Mouse_x, Mouse_y):
-                excelfunc.answertoDB(excelfunc.checkAnswer(2, Card_Num, Cube_Num), GameID)
-                print("A2")
-                count = 1
-            if A3.collidepoint(Mouse_x, Mouse_y):
-                excelfunc.answertoDB(excelfunc.checkAnswer(3, Card_Num, Cube_Num), GameID)
-                print("A3")
-                count = 1
-            if P.collidepoint(Mouse_x, Mouse_y):
-               screen_Num=2
-
     elif screen_Num == 2:
+      tester_image = pygame.image.load("TesterMain.png").convert()
+      screen.blit(tester_image, [0, 0])
+    elif screen_Num == 3:
+      user_image = pygame.image.load("UserMain.png").convert()
+      screen.blit(user_image, [0, 0])
+      if event.type == pygame.MOUSEBUTTONUP:
+       pos = pygame.mouse.get_pos()
+      if(pos):
+       print(pos)
+          #the Rules Button
+       if((pos[0]>235 and pos[0]<419) and (pos[1]>229 and pos[1]<296) and pygame.mouse.get_pressed()[0]):
+          subScreen_num=1
+          pos=False
+           #The StartGame Button
+       if ((pos[0] > 501 and pos[0] < 681) and (pos[1] > 238 and pos[1] < 301) and pygame.mouse.get_pressed()[0]):
+           screen_Num = 4
+           #Exit Func
+       if ((pos[0] > 12 and pos[0] < 160) and (pos[1] > 7 and pos[1] < 42) and pygame.mouse.get_pressed()[0]):
+           pygame.quit()
+      if subScreen_num==1:
+         rules_image = pygame.image.load("Rules.png").convert()
+         screen.blit(rules_image, [0, 0])
+         if event.type == pygame.MOUSEBUTTONUP:
+          pos = pygame.mouse.get_pos()
+          if pos:
+            print(pos)
+            if((pos[0]>308 and pos[0]<418) and (pos[1]>12 and pos[1]<50)):
+                subScreen_num = 0
+
+
+    #Board Game!!
+    elif screen_Num == 4:
+        game_image = pygame.image.load("Game.png").convert()
+        screen.blit(game_image, [0, 0])
+        if event.type == pygame.MOUSEBUTTONUP:
+            pos = pygame.mouse.get_pos()
+        if (pos):
+            print(pos)
+            # the Rules Button
+            if ((pos[0] > 235 and pos[0] < 419) and (pos[1] > 229 and pos[1] < 296) and pygame.mouse.get_pressed()[0]):
+                subScreen_num = 1
+                pos = False
+
+
+    elif screen_Num == 5:
         screen.fill(WHITE)
         pygame.draw.rect(screen, BLACK, [Center_x, Center_y, A_wid, A_hig], 2)
         text = font.render("Continue?", True, BLACK)
