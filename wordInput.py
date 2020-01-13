9366c8633c357ecab0a2f02bfa1c2df10b9dcbfa
EladLap <eladlp@walla.com>
2020-01-13 17:28:27 +0200

    syntax change

--- wordInput.py
+++ wordInput.py
@@ -6,20 +6,16 @@ from datetime import date
 
 
 def wordInput(name, date, num1, num2):
+    workbook = openpyxl.load_workbook("gameSQL.xlsx")
+    sheet = workbook['Report']
     if num2 < 2:
-        rep = 'Your color vision regarded as normal. ' \
-              'You do not need a cure for color blindness.'
+        rep = str(sheet['B'+str(2)].value)
 
     elif 2 <= num2 < 4:
-        rep = 'Your color vision regarded as deficient. ' \
-              'Special contact lenses and glasses may help people who are color blind tell the difference between ' \
-              'colors. '
+        rep = str(sheet['B'+str(3)].value)
 
     else:
-        rep = 'Your color vision regarded as badly worsened. ' \
-              'You can use visual aids, apps, and other technology to help you live with color blindness. ' \
-              'For example, you can use an app to take a photo with your phone or tablet and then tap on part of the ' \
-              'photo to find out the color of that area. '
+        rep = str(sheet['B'+str(4)].value)
 
     doc = DocxTemplate("Report.docx")
     context = {'name': name, 'date': date, 'trueAns': num1, 'falseAns': num2, 'report': rep}
@@ -94,3 +90,5 @@ def menu():  # without arguments!!!!!!!!!!!!!
                 list2.append(sheet1['C' + str(i)].value)
 
         wordInput(list2[0], list2[1], list2[2], list2[3])
+
+menu()
